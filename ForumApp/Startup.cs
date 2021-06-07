fe209ed8b814da82983e4ca02a24ea634bb07fd7
Hapan9 <49860805+Hapan9@users.noreply.github.com>
2021-06-07 17:06:13 +0300

    v 7.2
    Swagger with auth

--- ForumApp/Startup.cs
+++ ForumApp/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 namespace PL
 {
@@ -63,7 +64,42 @@ namespace PL
                     };
                 });
 
-            services.AddSwaggerGen();
+            services
+                .AddSwaggerGen(swagger =>
+                {
+                    //This is to generate the Default UI of Swagger Documentation  
+                    swagger.SwaggerDoc("v1", new OpenApiInfo
+                    {
+                        Version = "v1",
+                        Title = "Forum",
+                        Description = "EPAM project"
+                    });
+                    // To Enable authorization using Swagger (JWT)  
+                    swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                    {
+                        Name = "Authorization",
+                        Type = SecuritySchemeType.ApiKey,
+                        Scheme = "Bearer",
+                        BearerFormat = "JWT",
+                        In = ParameterLocation.Header,
+                        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+                    });
+                    swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+                    {
+                        {
+                              new OpenApiSecurityScheme
+                                {
+                                    Reference = new OpenApiReference
+                                    {
+                                        Type = ReferenceType.SecurityScheme,
+                                        Id = "Bearer"
+                                    }
+                                },
+                                new string[] {}
+
+                        }
+                    });
+                });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
